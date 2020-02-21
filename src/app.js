9700fd49db9bf3b57f834133633f33d38065d9da
‘peter-robert-cole’ <petercole3@gmail.com>
2020-02-20 23:40:48 -0800

    coming back

--- src/app.js
+++ src/app.js
@@ -1,60 +1,60 @@
 // Third-party resources
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
 // App-level middleware
-const app = express()
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
 
-const Memes = require('./models/memes')
-const memes = new Memes()
-const Users = require('./models/users')
-const users = new Users()
+const Memes = require('./models/memes');
+const memes = new Memes();
+const Users = require('./models/users');
+const users = new Users();
 // What we basically want is that /<model> ends up using the right model
-function getModel (req, res, next) {
-  const model = req.params.model
+function getModel(req, res, next) {
+  const model = req.params.model;
   // How can we get the right model into the req.params function?
   switch (model) {
     case 'memes':
-      req.model = memes
-      next()
-      break
+      req.model = memes;
+      next();
+      break;
     case 'users':
-      req.model = users
-      next()
-      break
+      req.model = users;
+      next();
+      break;
     default:
-      throw new Error('Invalid Model')
+      throw new Error('Invalid Model');
   }
 }
 
 // Routes
-app.param('model', getModel)
-app.get('/:model', handleGetAll)
-app.post('/:model', handlePost)
+app.param('model', getModel);
+app.get('/:model', handleGetAll);
+app.post('/:model', handlePost);
 
-function handleGetAll (req, res, next) {
+function handleGetAll(req, res, next){
   req.model.read()
     .then(result => {
       const output = {
         count: result.length,
         data: result
-      }
-      res.status(200).json(output)
+      };
+      res.status(200).json(output);
     })
     .catch(next)
-}
+};
 
-function handlePost (req, res, next) {
+function handlePost(req, res, next){
   req.model.create(req.body)
     .then(result => {
-      res.status(201).json(result)
+      res.status(201).json(result);
     })
-    .catch(next)
-}
+    .catch(next);
+};
 
 // Routes
 // const memesRouter = require('./api/memesRouter')
@@ -63,27 +63,27 @@ function handlePost (req, res, next) {
 // app.use(usersRouter)
 
 app.get('/this_will_error', (req, res) => {
-  throw new Error('yo dawg I heard you like errors so I put some error in your errors so you can error while you error')
+  throw new Error('Error for sure');
 })
 
 // Catch-alls
-const notFoundHandler = require('./middlewares/404')
-app.use(notFoundHandler)
-const internalServerErrorHandler = require('./middlewares/internalServerErrorHandler')
-app.use(internalServerErrorHandler)
+const notFoundHandler = require('./middlewares/404');
+app.use(notFoundHandler);
+const internalServerErrorHandler = require('./middlewares/internalServerErrorHandler');
+app.use(internalServerErrorHandler);
 
-let isRunning = false
+let isRunning = false;
 
 module.exports = {
   server: app,
   start: function (port) {
-    if (!isRunning) {
+    if (!isRunning){
       app.listen(port, () => {
-        isRunning = true
-        console.log(`Server listening on port ${port}...`)
+        isRunning = true;
+        console.log(`Server listening on port ${port}...`);
       })
     } else {
-      console.error('Server is already running!')
+      console.error('Server is already running!');
     }
   }
-}
+};
